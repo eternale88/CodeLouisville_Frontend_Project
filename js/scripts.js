c9a477472496522c501e63cb8a86445f9de0950b
eternale88 <ethanrobbins@ymail.com>
2017-03-09 19:12:45 -0500

    fixed footer

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,13 @@
 $(document).ready(function(){
     $("#self-image").hide().show("slow");
 });
+
+
+/* Code for Widget that displays treehouse progress-credit-
+ https://github.com/rileyhilliard/reportcard */
+
+ $('.report-card.treehouse').reportCard({
+    userName: 'ethanrobbins',
+    site: 'treehouse',
+    badgesAmount: 7
+});
